e35cde207821e66d42a03e5e4f9c26b218b0950b
Antuane_Specione <boosla@gmail.com>
2024-08-27 01:28:35 +0300

    feat: add minute to responder

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ API_HASH = config("API_HASH")
 app = Client("auto_responder_bot", api_id=API_ID, api_hash=API_HASH)
 
 start_hour = 14
-end_hour = 2
+end_hour = 1
+end_minute = 30
 timezone_offset = 3 
 
 @app.on_message(filters.private)
@@ -16,11 +17,10 @@ async def auto_response(client, message):
     now = datetime.now(timezone.utc)
     local_time = now + timedelta(hours=timezone_offset)
     now_hour = local_time.hour
+    now_minute = local_time.minute
     now_weekday = local_time.weekday() 
-    if (now_hour >= end_hour and now_hour < start_hour) or now_weekday in [5, 6]: 
-        await message.reply("Извините, я сейчас недоступен. Пожалуйста, свяжитесь со мной с 14:00 до 01:00 по UTC+3 в будние дни.")
-    # else:
-    #     await message.reply("Спасибо за ваше сообщение. Я постараюсь ответить вам как можно скорее!")
+    if (now_hour > end_hour or (now_hour == end_hour and now_minute >= end_minute)) and now_hour < start_hour or now_weekday in [5, 6]:
+        await message.reply("Извините, я сейчас недоступен. Пожалуйста, свяжитесь со мной с 14:00 до 01:30 по UTC+3 в будние дни.")
 
 if __name__ == "__main__":
     print("Bot is running...")
